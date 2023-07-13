7d816d2f41b2169d0dbaaa92e605fa677d1edf51
Matheus-Dev <darkaixxt@gmail.com>
2023-07-12 23:46:12 -0300

    Adiciona novos campos na tabela de cliente

--- integration/core/models.py
+++ integration/core/models.py
@@ -2,20 +2,16 @@ from django.db import models
 
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=100)
-    telefone = models.CharField(max_length=20)
-    email = models.EmailField(max_length=100)
-    data_nascimento = models.DateField(null=True, blank=True)
-    genero = models.CharField(max_length=10, null=True, blank=True)
-    cpf_cnpj = models.CharField(max_length=20, null=True, blank=True)
-    estado_civil = models.CharField(max_length=20, null=True, blank=True)
-    profissao = models.CharField(max_length=100, null=True, blank=True)
-    data_cadastro = models.DateTimeField(auto_now_add=True)
-    ultima_atualizacao = models.DateTimeField(auto_now=True)
-    cep = models.CharField(max_length=20, null=True, blank=True)
-    logradouro = models.CharField(max_length=255, null=True, blank=True)
-    numLogr = models.CharField(max_length=255, null=True, blank=True)
-    complLogr = models.CharField(max_length=255, null=True, blank=True)
-    bairro = models.CharField(max_length=255, null=True, blank=True)
-    cidade = models.CharField(max_length=255, null=True, blank=True)
-    estado = models.CharField(max_length=10, null=True, blank=True)
+    id = models.BigAutoField(primary_key=True)
+    cpf = models.CharField(max_length=11)
+    nome = models.CharField(max_length=150)
+    dt_nascimento = models.DateField(null=True, blank=True)
+    especie = models.CharField(max_length=100)
+    matricula = models.CharField(max_length=20)
+    telefone1 = models.CharField(max_length=20)
+    telefone2 = models.CharField(max_length=20)
+    telefone3 = models.CharField(max_length=20)
+    observacoes = models.TextField()
+
+    def __str__(self):
+        return self.nome
