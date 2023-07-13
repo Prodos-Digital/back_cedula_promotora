a491a229344d0a667fbe5d1538616a677889b0fa
Matheus-Dev <darkaixxt@gmail.com>
2023-07-12 23:46:30 -0300

    Cria view para clientes

---
+++ integration/core/views/clientes.py
@@ -0,0 +1,92 @@
+import json
+import pandas as pd
+from datetime import datetime
+
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from integration.core.models import Cliente
+from integration.core.serializer import ClienteMS
+
+
+class ClientesViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteMS
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        serializer = super().get_serializer_class()
+        return serializer
+
+    def list(self, request):
+
+        try:
+            clientes = Cliente.objects.all()
+            serializer = ClienteMS(clientes, many=True)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        except Exception as err:
+            print("ERROR>>>", err)
+            return Response(data={'success': False, 'message': str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk):
+
+        try:
+            cliente = Cliente.objects.get(cpf=pk)
+            serializer = ClienteMS(cliente)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        except Exception as err:
+            print("ERROR>>>", err)
+            return Response(data={'success': False, 'message': str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request):
+
+        try:
+            serializer = ClienteMS(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as err:
+            print("ERROR>>>", err)
+            return Response(data={'success': False, 'message': str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk):
+
+        try:
+            cliente = Cliente.objects.get(cpf=pk)
+            serializer = ClienteMS(instance=cliente, data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as err:
+            print("ERROR>>>", err)
+            return Response(data={'success': False, 'message': str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+
+        cpf = request.GET.get("cpf")
+
+        try:
+            cliente = Cliente.objects.get(cpf=cpf)
+            cliente.delete()
+
+            return Response(status=status.HTTP_200_OK)
+
+        except Exception as err:
+            print("ERROR>>>", err)
+            return Response(data={'success': False, 'message': str(err)}, status=status.HTTP_400_BAD_REQUEST)
