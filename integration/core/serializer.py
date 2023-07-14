4cd5fa9a323c5c7e1d363641c740558155f17d21
Matheus-Dev <darkaixxt@gmail.com>
2023-07-13 22:33:31 -0300

    Cria novo Serializer para contratos

--- integration/core/serializer.py
+++ integration/core/serializer.py
@@ -6,3 +6,8 @@ class ClienteMS(serializers.ModelSerializer):
     class Meta:
         model = Cliente 
         fields = '__all__'
+
+class ContratoMS(serializers.ModelSerializer):
+    class Meta:
+        model = Contrato 
+        fields = '__all__'
