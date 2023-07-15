098c4d204818e5c4d1ad447639d02615c77abf30
Matheus-Dev <darkaixxt@gmail.com>
2023-07-15 11:41:14 -0300

    Adiciona novas colunas no model de despesas

--- integration/core/models.py
+++ integration/core/models.py
@@ -38,6 +38,8 @@ class Despesa(models.Model):
     descricao = models.CharField(max_length=255, null=True, blank=True)
     valor = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     situacao = models.CharField(max_length=100, null=True, blank=True)
+    tp_despesa = models.CharField(max_length=100, null=True, blank=True)
+    natureza_despesa = models.CharField(max_length=100, null=True, blank=True)
 
 class Emprestimo(models.Model):
     id = models.BigAutoField(primary_key=True)
