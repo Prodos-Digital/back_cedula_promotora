0097e59b551d48103b6576680543e6381b56ea51
glayson visgueira <glaysonwow@gmail.com>
2024-04-22 18:59:41 -0300

    Fix totalizadores do dash de clientes

--- integration/core/usecases/clientes.py
+++ integration/core/usecases/clientes.py
@@ -21,32 +21,33 @@ class DashboardClientes():
             TOTALIZADOR DE CLIENTES AGRUPADOS POR ESPÉCIE DO INSS
         '''
 
-        print(data)
-
         df = pd.DataFrame.from_dict(data)
         pd.set_option('display.expand_frame_repr', False)    
 
         if df.empty: 
              return self.empty_object() 
         
-        tt_count_clientes = df[['id']].count()
-        tt_count_clientes_sem_especies = df.loc[df['especie'].notna() & (df['especie'] != ''), 'id'].count()
-
-        print('tt_count_clientes ',tt_count_clientes)
-        print('tt_count_clientes_sem_especies ',tt_count_clientes_sem_especies)
         
-        clientes = df.groupby(['especie'], as_index=False)['id'].agg(['count']).rename(columns={'count': 'qtd'}).sort_values(by=['qtd'], ascending=False).reset_index()
-        clientes['perc_qtd'] = round(clientes['qtd']/clientes['qtd'].sum() * 100,2)
-        tt_clientes = clientes.to_dict('records')
+        tt_clientes_especies = df[['id']].count()
+        tt_clientes_sem_especies = df.loc[df['especie'].notna() & (df['especie'] != ''), 'id'].count()
+
+        # Remover as linhas onde a coluna 'especie' é nula ou vazia ("")
+        df = df.dropna(subset=['especie'])
+        df = df.drop(df[df['especie'] == ""].index)
+        
+        # Totalizar as espécies
+        especies = df.groupby(['especie'], as_index=False)['id'].agg(['count']).rename(columns={'count': 'qtd'}).sort_values(by=['qtd'], ascending=False).reset_index()
+        especies['perc_qtd'] = round(especies['qtd']/especies['qtd'].sum() * 100,2)
+        tt_especies = especies.to_dict('records')
        
         #breakpoint()
 
         return {
             'indicadores': {
-                'clientes': tt_clientes,
-                'tt_clientes': {
-                    'total': tt_count_clientes,
-                    'sem_especie': tt_count_clientes_sem_especies
+                'especies': tt_especies,
+                'tt_especies': {
+                    'total': tt_clientes_especies,
+                    'sem_especie': tt_clientes_sem_especies
                 }
             },            
         }
