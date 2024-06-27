cc58cac126711f0cb3412b7445487510e9722f4b
glayson visgueira <glaysonwow@gmail.com>
2024-06-26 22:41:16 -0300

    Funcao no repository para get no dados do cliente

--- integration/emprestimos/repository/clientes.py
+++ integration/emprestimos/repository/clientes.py
@@ -21,4 +21,23 @@ class ClientesRepository():
             data = dictfetchall(cursor)
 
         return data if data else []
+    
+    def get_dados_cliente(self, cpf=None): 
+
+        SQL = f"""           
+                SELECT
+                   *
+                FROM
+                    emp_emprestimos ee
+                WHERE cpf = '{cpf}' 
+                ORDER BY ee.dt_emprestimo DESC;
+            """   
+           
+        
+        with connection.cursor() as cursor:   
+
+            cursor.execute(SQL)
+            data = dictfetchall(cursor)
+
+        return data if data else []
   
